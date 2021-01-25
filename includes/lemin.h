22460d5e5882daf47b798b08ce9d49b95b831971
KatyaPrusakova <katia.prusakova@gmail.com>
2021-01-25 22:09:36 +0200

    new parsing under construction, queue linked list deleted

--- includes/lemin.h
+++ includes/lemin.h
@@ -6,7 +6,7 @@
 /*   By: eprusako <eprusako@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 11:32:15 by eprusako          #+#    #+#             */
-/*   Updated: 2021/01/24 18:17:04 by eprusako         ###   ########.fr       */
+/*   Updated: 2021/01/25 21:53:42 by eprusako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,32 @@
 typedef struct s_room
 {
 	char	*name;
+	int		index;
+	int		prev_room_index;
 	int		s;
 	int		e;
 	int		visited;
 	struct s_room	*next;
 } t_room;
 
-typedef struct s_queue {
-	int *waiting_list;
-	int i;
-	int front;
-	int rear;
-} t_queue;
+// typedef struct s_queue {
+// 	int		index;
+// 	char	*name;
+// 	struct s_queue	*next;
+// } t_queue;
+
+
 
 typedef struct s_graph 
 {
 	int		ants;
-	int		start;
+	int		start; // bad idea, change start to 0 note
 	int		end;
 	int		room_count;
 	char	*room_name;
+	int		*queue;
 	char	*line;
 	t_room	**adlist;
-	t_queue *q;
 } t_graph;
 
 int		ft_error(t_graph* data, int opt);
--- src/lemin.c
+++ src/lemin.c
@@ -6,7 +6,7 @@
 /*   By: eprusako <eprusako@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 15:03:05 by eprusako          #+#    #+#             */
-/*   Updated: 2021/01/24 19:05:06 by eprusako         ###   ########.fr       */
+/*   Updated: 2021/01/25 22:00:09 by eprusako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ int				link_rooms(char *room, int i, t_graph* data)
 
 	new = create_room(data);
 	tmp = data->adlist[i];
-	
+
 	if (tmp->next)
 	{
 		while (tmp->next != NULL)
@@ -84,58 +84,62 @@ int				link_rooms(char *room, int i, t_graph* data)
 	return (1);
 }
 
-t_queue	*create_queue(int size, t_queue *new)
-{
-	new = ft_memalloc(sizeof(t_queue));
-	new->waiting_list = ft_memalloc(sizeof(size));
-	return (new);
-}
-
-// int			sort_graph(t_graph* data)
+// t_queue	*create_queue(int size, t_queue *new)
 // {
-// 	t_room	*tmp;
-// 	int		head;
-// 	int		end;
-// 	while (graph->adlist[i])
+// 	t_queue	*tmp;
+// 	t_queue	*head;
+// 	new = ft_memalloc(sizeof(t_queue));
+// 	head = new;
+// 	while (size--)
 // 	{
-// 		if (data->adlist[i]->s && i != 0)
-// 			{
-// 				tmp = data->adlist[i];			
-// 			}
-// 		if (tmp->next)
-// 		{
-// 			while (tmp->next != NULL)
-// 				tmp = tmp->next;
-// 		}
+// 		tmp = ft_memalloc(sizeof(t_queue));
+// 		//new->index = 1;
+// 		new->next = tmp;
+// 		new = new->next;
 // 	}
+// //	printf("new->index %d\n", head->index);
+// 	return (head);
 // }
 
 // Adding elements into queue
-void			add_to_queue(t_queue *q, int room_number)
-{
-	int i;
+// void			add_to_queue(t_queue *q, t_room	*room)
+// {
+// 	q->name = room->name;
+// 	if (room->next) // && not in queue already
+// 	{
+// 		//
+// 		while (room->next)
+// 		{
+// 			q = q->next;
+// 			room = room->next;
+// 			q->name = room->name;
+// 		}
+// 	}
+// //	printf("q->i %d\n", q->i);
+// }
 
-	i = 0;
-	q->waiting_list[q->i] = room_number;
-	while (q->waiting_list[i]) //delete
-	{
-		printf("queue is %d\n", q->waiting_list[i]);
-		i++;
-	}
-	q->i++;
-}
+// void			print_queue(t_queue *q, int i)
+// {
+// 	while (q->next)
+// 	{
+// 		printf("queue note number %d q->name %s\n", i, q->name);
+// 		q = q->next;
+// 		i++;
+// 	}
+// }
 
-void			remove_from_queue(t_queue *q, int room_number)
-{
-	q->waiting_list[room_number] = NULL;
-}
+// void			remove_from_queue(t_queue *q, int room_number)
+// {
+// 	q->index = q->index;
+// 	//remove link from linked list
+// }
 
-int			queue_is_empty(t_queue *q)
-{
-	if (q->waiting_list)
-		return (1);
-	return (0);
-}
+// int				queue_is_empty(t_queue *q)
+// {
+// 	if (q->next == NULL)
+// 		return (1);
+// 	return (0);
+// }
 
 int				find_index_name_room(t_graph* data, char *s)
 {
@@ -151,25 +155,29 @@ int				find_index_name_room(t_graph* data, char *s)
 	return(ft_error(data, 1));
 }
 
-int				bfs(t_graph* data, int start_i)
-{
-	t_queue		*q;
-	t_room		*tmp;
+// int				bfs(t_graph* data, int start_i)
+// {
+// 	t_queue		*q;
+// 	t_room		*tmp;
 
-	q = create_queue(data->room_count, data->q);
-	data->adlist[start_i]->visited = 1;
-	tmp = data->adlist[start_i];
-
-	add_to_queue(q, start_i);
-	while (!queue_is_empty(q))
-	{
-		start_i = find_index_name_room(data, tmp->name);
-		printf("index of room %d\n", start_i);
-		add_to_queue(q, start_i);
-		tmp = tmp->next;
-	}
-	return (1);
-}
+// 	q = create_queue(data->room_count, data->q);
+// 	print_queue(q, 0);
+// 	tmp = data->adlist[start_i];
+// 	tmp->visited = 1;
+	
+// 	// while didn't reach all nodes
+// 		add_to_queue(q, tmp);
+// 	print_queue(q, 0);
+	
+// 	// while (!queue_is_empty)
+// 	// {
+// 	// 	start_i = find_index_name_room(data, tmp->name);
+// 	// 	printf("index of room %d\n", start_i);
+// 	// 	add_to_queue(q, start_i);
+// 	// 	tmp = tmp->next;
+// 	// }
+// 	return (1);
+// }
 
 int				ft_link(t_graph* data)
 {
@@ -179,7 +187,8 @@ int				ft_link(t_graph* data)
 
 	i = 0;
 	flag = 0;
-	room = ft_strsplit(data->line, '-');
+	room = ft_strsplit(data->line, '-'); 
+	//0-2 nana-lala nana = 1 ADD INDEX to notes
 	while (data->adlist[i] && i < data->room_count)
 	{
 		//printf("loop %d|%d |%s\n", len, i, data->adlist[i]->name);
@@ -238,34 +247,56 @@ int				ft_error(t_graph* data, int opt)
 	exit(0);
 }
 
+// t_graph*		parse_graph(char **av, t_graph* graph) 
+// {
+	
+// 	int			i;
+
+// 	i = 0;
+// 	av[1] = NULL; // delete
+// 	if (!(graph = create_graph(4))) // diffferent order, graph shall be created after input is read
+// 		ft_error(graph, 2);
+// 	if (ft_ants_num(graph))
+// 		ft_printf("%d\n", graph->ants);
+// 	while (get_next_line(0, &graph->line) == 1)
+// 	{
+// 		if (graph->line[0] == '#')
+// 			ft_comment(graph);
+// 		else if (ft_strchr(graph->line, ' ') && !(ft_strchr(graph->line, '-')) && !(ft_strchr(graph->line, '#')))
+// 			ft_room(graph);
+// 		else if (ft_strchr(graph->line, '-') && ft_link(graph))
+// 			ft_printf("%s\n", graph->line);
+// 		else
+// 			ft_error(graph, 1);
+// 		free(graph->line);
+// 		graph->line = NULL;
+// 	}
+// 	ft_printf("\n");
+// 	print_rooms(graph);
+// 	bfs(graph, 1);
+// 	return (0);
+// }
+
+
 t_graph*		parse_graph(char **av, t_graph* graph) 
 {
-	
-	int			i;
+	int		i;
+	char	**line;
 
 	i = 0;
-	av[1] = NULL; // delete
-	if (!(graph = create_graph(4)))
-		ft_error(graph, 2);
-	if (ft_ants_num(graph))
-		ft_printf("%d\n", graph->ants);
-	while (get_next_line(0, &graph->line) == 1)
+	av[0] = NULL; // delete if av not needed
+	line = ft_memalloc(sizeof(char*) * 100); // if goes to 101 malloc more
+	while (get_next_line(0, &line[i]) == 1)
 	{
-		if (graph->line[0] == '#')
-			ft_comment(graph);
-		else if (ft_strchr(graph->line, ' ') && !(ft_strchr(graph->line, '-')) && !(ft_strchr(graph->line, '#')))
-			ft_room(graph);
-		else if (ft_strchr(graph->line, '-') && ft_link(graph))
-			ft_printf("%s\n", graph->line);
-		else
-			ft_error(graph, 1);
-		free(graph->line);
-		graph->line = NULL;
+		i++;
 	}
-	ft_printf("\n");
-	print_rooms(graph);
-	bfs(graph, 1);
-	return (0);
+	i = 0;
+	while (line[i])
+	{
+		ft_printf("%s\n", line[i]);
+		i++;
+	}
+	return (graph); // change
 }
 
 int main(int argc, char **argv)
