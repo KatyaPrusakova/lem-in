5f072ed1bf6d710dd5940fa456eb91850b0b4600
KatyaPrusakova <katia.prusakova@gmail.com>
2021-02-15 13:06:08 +0200

    segfault in parse_input fixed

--- src/parse_input.c
+++ src/parse_input.c
@@ -6,18 +6,18 @@
 /*   By: eprusako <eprusako@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 15:03:05 by eprusako          #+#    #+#             */
-/*   Updated: 2021/02/14 21:36:09 by eprusako         ###   ########.fr       */
+/*   Updated: 2021/02/15 13:04:25 by eprusako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-char	**ft_realloc(char **input, int fd)
+char	**ft_realloc(char **input, int len)
 {
 	char **tmp;
 
-	tmp = ft_memalloc(sizeof(char*) * (fd * 2));
-	tmp = ft_memcpy(tmp, input, sizeof(char*) * fd);
+	tmp = ft_memalloc(sizeof(char*) * (len * 2 + 1));
+	tmp = ft_memcpy(tmp, input, sizeof(char*) * len);
 	free(input);
 	return(tmp);
 }
@@ -26,14 +26,12 @@ char		**parse_input(void)
 {
 	int i;
 	int len;
-//	int fd;// delete
 	char *line;
 	char **input;
 
 	i = 0;
 	len = 5;
-//	fd = open(av[1], O_RDWR);//delete
-	input = ft_memalloc(sizeof(char*) * len);
+	input = ft_memalloc(sizeof(char*) * len + 1);
 	while (get_next_line(0, &line))
 	{
 		//ft_dprintf(fd, "%s\n", line); 
@@ -45,7 +43,6 @@ char		**parse_input(void)
 		input[i] = line;
 		i++;
 	}
-	input[i] = NULL;
 	return (input);
 }
 
