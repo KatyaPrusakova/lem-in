227923c1ecedf5aee5bb49827014b7b435d20f74
ksuomala <ksuomala@student.hive.fi>
2021-02-15 16:27:46 +0200

    removed extra print

--- src/parse_input.c
+++ src/parse_input.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eprusako <eprusako@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 15:03:05 by eprusako          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/02/15 16:10:14 by ksuomala         ###   ########.fr       */
-=======
-/*   Updated: 2021/02/15 14:27:06 by eprusako         ###   ########.fr       */
->>>>>>> 5376269ca7b8f019f95e28c21e1d0808300c5ace
+/*   Updated: 2021/02/15 16:27:20 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +46,6 @@ char		**parse_input(void)
 	input = ft_memalloc(sizeof(char*) * len + 1);
 	while (get_next_line(0, &line) == 1)
 	{
-<<<<<<< HEAD
-		ft_printf("%s\n", line);
-=======
-		//ft_dprintf(fd, "%s\n", line);
->>>>>>> 5376269ca7b8f019f95e28c21e1d0808300c5ace
 		if (i == len)
 		{
 			input = ft_realloc(input, len);
@@ -63,7 +54,7 @@ char		**parse_input(void)
 		input[i] = line;
 		i++;
 	}
-	// if (len == 5)  // file should have at least 6 lines 
+	// if (len == 5)  // file should have at least 6 lines
 	// 	print_error(1);
 	return (input);
 }
