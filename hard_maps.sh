8c63cf65b9ef22a3ba57ebd796b596839c62dd10
ksuomala <ksuomala@ksuomalas-iMac.local>
2021-04-12 08:21:10 -0700

    script

--- hard_maps.sh
+++ hard_maps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-test_dir="script_maps/"
-flow_one=$test_dir"f1"
+test_dir="script_maps"
+flow_one="$test_dir/f1"
 
 mkdir -p $test_dir
 
@@ -11,11 +11,13 @@ index="0"
 
 for i in {1..10}
 do
-	./lem-in < $flow_one > $test_diroutput.txt
+	echo run
+	./lem-in < $flow_one > "$test_dir/output.txt"
 	while IFS= read -r line
 	do
+		echo "read"
 		echo $index
-		index = $($index+1)
+		index = $(($index+1))
 	done
 done
 
