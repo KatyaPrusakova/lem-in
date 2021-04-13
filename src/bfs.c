cd3d7db2ed631c4ceb0f1baa840ffd45cc7c9cca
ksuomala <ksuomala@student.hive.fi>
2021-04-13 14:20:55 +0300

    stop searching for new paths in a set when the found path is too long

--- src/bfs.c
+++ src/bfs.c
@@ -6,7 +6,7 @@
 /*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 15:25:06 by ksuomala          #+#    #+#             */
-/*   Updated: 2021/04/10 19:19:09 by ksuomala         ###   ########.fr       */
+/*   Updated: 2021/04/13 14:15:29 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,8 @@ void	visit_room(t_room *current, t_queue *q, int *visited, t_graph *graph, int s
 		tmp = tmp->next;
 	}
 	if(graph->visualize)
-	{
-		visited_to_visualizer(current->index, current->prev_room_index, 1);
-		queue_to_visualizer(graph->adlist, q, 1);
-	}
-	//test
+		visualize_search(current, q);
+		//test
 //	ft_dprintf(fd, "\n");
 	// for (int i = 0; i < graph->room_total; i++)
 	// {
@@ -114,8 +111,8 @@ t_path	*bfs(t_graph *graph, int edge_w)
 		if (end_is_neighbour(tmp) && visited[room->index] == -1 && \
 		check_weight(graph->weight_m[room->index][graph->room_total - 1], edge_w))
 		{
-			visited_to_visualizer(room->index, room->prev_room_index, graph->visualize);
-			queue_to_visualizer(graph->adlist, q, graph->visualize);
+			if (graph->visualize)
+				visualize_search(room, q);
 			visited[room->index] = room->prev_room_index;
 			return (save_path(visited, room->index, graph, graph->room_total - 1));
 		}
@@ -153,10 +150,16 @@ t_path	**bfs_set(t_graph *graph, int edge_w, t_path **set, int max_paths)
 		if (end_is_neighbour(tmp) && visited[room->index] == -1 && \
 		check_weight(graph->weight_m[room->index][graph->room_total - 1], edge_w))
 		{
-			visited_to_visualizer(room->index, room->prev_room_index, graph->visualize);
-			queue_to_visualizer(graph->adlist, q, graph->visualize);
+			if (graph->visualize)
+				visualize_search(room, q);
 			visited[room->index] = room->prev_room_index;
 			check_path(graph, visited, room->index, set, &path_no);
+			if (path_no > 1 && set[path_no - 1]->len > set[path_no - 2]->len + (graph->ants - path_no))
+			{
+				ft_printf("PATH IS TOO LONG");
+				set[path_no - 1] = free_path(set[path_no - 1]);
+				return (set);
+			}
 		}
 		else
 			visit_room(room, q, visited, graph, edge_w);
@@ -165,36 +168,36 @@ t_path	**bfs_set(t_graph *graph, int edge_w, t_path **set, int max_paths)
 	return (set);
 }
 
-t_path	**bfs_set_modify(t_graph *graph, int edge_w, t_path **set)
-{
-	t_queue	*q;
-	t_room	*tmp;
-	t_room	*room;
-	t_path	*to_mod;
-	int		*visited;
+// t_path	**bfs_set_modify(t_graph *graph, int edge_w, t_path **set)
+// {
+// 	t_queue	*q;
+// 	t_room	*tmp;
+// 	t_room	*room;
+// 	t_path	*to_mod;
+// 	int		*visited;
 
-	q = NULL;
-	visited = init_visited(graph->room_total);
-	q = enqueue(0, q, graph->adlist, 0);
-	while (q->head)
-	{
-		room = ft_memdup(graph->adlist[q->head->index], sizeof(t_room));
-		room->prev_room_index = q->head->prev_room_index;
-		if (q->head)
-			dequeue(q);
-		tmp = room->next;
-		if (end_is_neighbour(tmp) && visited[room->index] == -1 && \
-		check_weight(graph->weight_m[room->index][graph->room_total - 1], edge_w))
-		{
-			visited_to_visualizer(room->index, room->prev_room_index, graph->visualize);
-			queue_to_visualizer(graph->adlist, q, graph->visualize);
-			visited[room->index] = room->prev_room_index;
-			to_mod = mod_path(visited, room->index, graph->room_total - 1);
-			mod_edgeweight_path(graph->weight_m,to_mod, graph, 0);
-		}
-		else
-			visit_room(room, q, visited, graph, edge_w);
-		ft_memdel((void**)&room);
-	}
-	return (set);
-}
+// 	q = NULL;
+// 	visited = init_visited(graph->room_total);
+// 	q = enqueue(0, q, graph->adlist, 0);
+// 	while (q->head)
+// 	{
+// 		room = ft_memdup(graph->adlist[q->head->index], sizeof(t_room));
+// 		room->prev_room_index = q->head->prev_room_index;
+// 		if (q->head)
+// 			dequeue(q);
+// 		tmp = room->next;
+// 		if (end_is_neighbour(tmp) && visited[room->index] == -1 &&
+// 		check_weight(graph->weight_m[room->index][graph->room_total - 1], edge_w))
+// 		{
+// 			visited_to_visualizer(room->index, room->prev_room_index, graph->visualize);
+// 			queue_to_visualizer(graph->adlist, q, graph->visualize);
+// 			visited[room->index] = room->prev_room_index;
+// 			to_mod = mod_path(visited, room->index, graph->room_total - 1);
+// 			mod_edgeweight_path(graph->weight_m,to_mod, graph, 0);
+// 		}
+// 		else
+// 			visit_room(room, q, visited, graph, edge_w);
+// 		ft_memdel((void**)&room);
+// 	}
+// 	return (set);
+// }
--- src/visualize.c
+++ src/visualize.c
@@ -6,7 +6,7 @@
 /*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 16:19:16 by ksuomala          #+#    #+#             */
-/*   Updated: 2021/03/30 16:35:47 by ksuomala         ###   ########.fr       */
+/*   Updated: 2021/04/13 14:14:55 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@
 ** Prints the current state of queue to stdout.
 */
 
-void	queue_to_visualizer(t_room **adlist, t_queue *q, int visualize)
+void	visualize_search(t_room *room, t_queue *q)
 {
 	t_room	*tmp;
 
-	if (!visualize)
-		return;
-	if (!adlist)
-		ft_printf("unused variable");
+	ft_printf("%d %d\n", room->index, room->prev_room_index);
 	tmp = q->head;
 	if (!q || !tmp)
 		return;
@@ -33,12 +30,4 @@ void	queue_to_visualizer(t_room **adlist, t_queue *q, int visualize)
 		tmp = tmp->next;
 	}
 	ft_n(1);
-}
-
-
-void	visited_to_visualizer(int current_index, int visited_from, int visualize)
-{
-	if (!visualize)
-		return;
-	ft_printf("%d %d\n", current_index, visited_from);
 }
